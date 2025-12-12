b8867fd66e1b2f506ce62825ec6103a7cb147525
garrycnx <garrycnx@gmail.com>
2025-12-12 23:31:18 +0530

    Updated scheduler logic and bug fixes

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ def min_to_time(m):
 # UI
 # ---------------------------
 st.title("AI Schedules Generator by Data Quest")
-
+st.title("Developed by Gurpreet Singh")
 with st.sidebar:
     st.header("Settings")
     uploaded = st.file_uploader("Upload forecast CSV (one week, DD-MM-YYYY)", type=["csv"])
